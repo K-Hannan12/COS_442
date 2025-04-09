59e7072c1105f20c9f833a27f93324c4e338738f
Kaleb Hannan <kaleb.hannan@maine.edu>
2025-04-09 18:14:13 -0400

    create weatherman and fix nginx config

--- HW5/Weatherman/weatherman.py
+++ HW5/Weatherman/weatherman.py
@@ -1,11 +1,15 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def weatherman():
-    return "weatherman"
+    responseTemp = requests.get("http://temp:5005/")
+    responseWind = requests.get("http://wind:5005/")
+    responseHumidity = requests.get("http://humidity:5005/")
+    return f'wind: {responseWind.text}\ntemp: {responseTemp.text}\nhumidity: {responseHumidity.text}'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5021)
+    app.run(host='0.0.0.0', port=5005)
