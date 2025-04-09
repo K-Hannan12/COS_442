4383b5d60813908b206b3954197b612d0bf466f1
Kaleb Hannan <kaleb.hannan@maine.edu>
2025-04-09 17:37:55 -0400

    finished temp.py

--- HW5/Temp/temp.py
+++ HW5/Temp/temp.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def temp():
-    return "Temp"
+    num = random.randint(-10, 110)
+    return f'temp = {num}'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5021)
+    app.run(host='0.0.0.0', port=5005)
