4fc26bb1912c927d4c2762f57e7f889924cd2456
Kaleb Hannan <kaleb.hannan@maine.edu>
2025-04-09 17:36:42 -0400

    finished humidity

--- HW5/Humidity/humidity.py
+++ HW5/Humidity/humidity.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def humidity():
-    return "humidity"
+    num = random.randint(0, 98)
+    return f'’humidity = {num}'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5021)
+    app.run(host='0.0.0.0', port=5005)
