b62100f1423f57f583c6ec934b3daea5825770a1
Kaleb Hannan <kaleb.hannan@maine.edu>
2025-04-18 13:34:54 -0400

    add fix

--- IC4/base/app.py
+++ IC4/base/app.py
@@ -1,9 +1,8 @@
 import socket
-
 import redis
 from flask import Flask, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__,  )
 cache = redis.Redis(host='redis', port=6379)
 
 def get_hit_count():
