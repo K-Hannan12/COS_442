0dab145567c0c5b94940817b7758b0512ab75c94
Kaleb Hannan <kaleb.hannan@maine.edu>
2025-04-20 12:07:50 -0400

    added reqierments.txt and created docker files

--- FinalProject/playerBio/PlayerBio.py
+++ FinalProject/playerBio/PlayerBio.py
@@ -7,9 +7,19 @@ redisDB = redis.Redis(host='redis', port=6379 , decode_responses=True)
 @app.route('/getplayerBio/<player_name>')
 def getplayerBio(player_name):
     # This will need to be changed to use the db correctly but is here as a placeholder
-    playerBio = redisDB.get(player_name)
-    if playerBio is None:
-        return 'Player not found', 404
+    playerBio = {
+    'name': 'Kaleb Hannan',
+    'age': '28',
+    'height': "6'2\"",
+    'weight': '210 lbs',
+    'position': 'Left Field',
+    'team': 'Red Sox',
+    'number': '12',
+    'headshot': 'https://via.placeholder.com/200x250.png?text=Headshot'
+}
+    #playerBio = redisDB.hget(player_name)
+    #if playerBio is None:
+    #    return 'Player not found', 404
     return str(playerBio), 200
 
 @app.route('/storeplayerBio/<player_name>/<player_bio>')
