be6f7fff7d2334c33a47fd91c988bc5da661a79c
Kaleb Hannan <kaleb.hannan@maine.edu>
2025-04-23 13:52:43 -0400

    save changes

--- FinalProject/playerBio/PlayerBio.py
+++ FinalProject/playerBio/PlayerBio.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask , request
 import redis
 
 app = Flask(__name__)
@@ -6,28 +6,30 @@ redisDB = redis.Redis(host='redis', port=6379 , decode_responses=True)
 
 @app.route('/getplayerBio/<player_name>')
 def getplayerBio(player_name):
-    # This will need to be changed to use the db correctly but is here as a placeholder
-    playerBio = {
-    'name': "Kaleb Hannan",
-    'age': "28",
-    'height': "6 feet 2 inches",
-    'weight': "210 lbs",
-    'position': "Left Field",
-    'team': "Red Sox",
-    'number': "12",
-    'bats': "Left",
-    'throws': "Left",
-    'headshot': 'https://via.placeholder.com/120x150.png?text=Kaleb'
-}
-    #playerBio = redisDB.hget(player_name)
-    #if playerBio is None:
-    #    return 'Player not found', 404
+    player_name = player_name.lower().replace(" ", "_")
+
+    #get the players information from the redis database
+    playerBio = redisDB.hget(f'{player_name}:bio') 
+    if playerBio is None:
+        return 'Player not found', 404
     return str(playerBio), 200
 
 @app.route('/storeplayerBio', methods=['POST'])
 def storeplayerBio():
-    # This will need to be changed to use the db correctly but is here as a placeholder
-    #redisDB.set(player_name, player_bio)
+    player_name = request.form['name'].lower().replace(" ", "_")
+    player_bio = {
+    'age': request.form['age'],
+    'height': request.form['height'],
+    'weight': request.form['weight'],
+    'position': request.form['position'],
+    'team': request.form['team'],
+    'number': request.form['number'],
+    'bats': request.form['bats'],
+    'throws': request.form['throws'],
+    'headshot': request.form['headshot']
+    }
+    #store the players bio information in the redis database
+    redisDB.hset(f'{player_name}:bio', mapping=player_bio)
     return 'Player bio stored', 200
 
 if __name__ == '__main__':
